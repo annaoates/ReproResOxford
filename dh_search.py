3d46c7a0c84afe484f252815c35b3d773dec879f
annaoates <annaireneo@gmail.com>
2017-07-05 12:18:26 +0100

    Adding DH Search Script

---
+++ dh_search.py
@@ -0,0 +1,19 @@
+# Script to search for an author in EEBO index
+
+import pandas as pd
+from ggplot import *
+
+g = pd.read_csv("data/TCP.csv")
+author = g.loc[g["Author"].str.contains("Luther", na=False)]
+freq = author["Date"].value_counts() # add a column
+
+# panda returns a dataframe; but counts returns a series
+# convert back to a dataframe
+
+n = pd.DataFrame({"Publication":freq.index, "Freq": freq.values})
+
+plt = ggplot( aes(x = "Publication", y = "Freq"), data = n) + geom_point() \
+	+ ggtitle("Works attributed to Luther") + xlab("Publication Date") \
+	+ ylab("Number of Publications")
+
+plt.save("Luther.png")
